c034dafb16d501d4a1d1bf8b94068b8cf35068f2
PALLE RAVINDRA KUMAR <119121995+ravindrakumar05@users.noreply.github.com>
2023-08-06 09:43:02 +0530

    files are are pushed at one time

---
+++ index.js
@@ -0,0 +1,6 @@
+function generateCat(){
+let ele = document.createElement('img');
+let parent = document.getElementById('flex-cat-gen')
+ele.src = "https://cdn2.thecatapi.com/images/189.gif";
+parent.appendChild(ele)
+}
